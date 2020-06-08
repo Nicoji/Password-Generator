36f74961682a38adaecc526f4e81613bc886701c
Nicolas Maillet <n.maillet07@gmail.com>
2020-06-08 18:23:15 +0200

    added script

--- script.js
+++ script.js
@@ -11,15 +11,28 @@ const saveButton = document.querySelector('.save');
 const infoPswSize = document.querySelector('small');
 
 // Variables
-const alphabet = "abcdefghijklmnopqrstuvwxz";
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const capsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberStr = "0123456789";
-const specialCharStr = "@$£+!?#&({[]})";
+const specialCharStr = "@$£€+!?#&({[]})";
 let password = '';
 
 // Functions
 const getRandomIndex = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+const shuffle = (string) => {
+    var sentence = string.split('');
+    var sentenceSize = sentence.length; 
+
+    for(i = sentenceSize; i > 0; i--) {
+        var index = Math.floor(Math.random() * (i + 1));
+        var letterOfSentence = sentence[i];
+        sentence[i] = sentence[index];
+        sentence[index] = letterOfSentence;
+    }
+    return sentence.join('');
 }
 
 const generatePswd = () => {
@@ -56,16 +69,16 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 4);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += capsStr.charAt(getRandomIndex(26));
+            password += capsStr.charAt(getRandomIndex(capsStr.length - 1));
         }
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += numberStr.charAt(getRandomIndex(10));
+            password += numberStr.charAt(getRandomIndex(numberStr.length - 1));
         }
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += specialCharStr.charAt(getRandomIndex(14));
+            password += specialCharStr.charAt(getRandomIndex(specialCharStr.length - 1));            
         }
         for(let i = 0; i < (pswSize.value - (numberOfIncludes * 3)); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -73,14 +86,14 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 3);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += capsStr.charAt(getRandomIndex(26));
+            password += capsStr.charAt(getRandomIndex(capsStr.length - 1));
         }
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += numberStr.charAt(getRandomIndex(10));
+            password += numberStr.charAt(getRandomIndex(numberStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - (numberOfIncludes * 2)); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -88,14 +101,14 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 3);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += capsStr.charAt(getRandomIndex(26));
+            password += capsStr.charAt(getRandomIndex(capsStr.length - 1));
         }
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += specialCharStr.charAt(getRandomIndex(14));
+            password += specialCharStr.charAt(getRandomIndex(specialCharStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - (numberOfIncludes * 2)); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -103,14 +116,14 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 3);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += numberStr.charAt(getRandomIndex(10));
+            password += numberStr.charAt(getRandomIndex(numberStr.length - 1));
         }
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += specialCharStr.charAt(getRandomIndex(14));
+            password += specialCharStr.charAt(getRandomIndex(specialCharStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - (numberOfIncludes * 2)); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -118,11 +131,11 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 2);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += capsStr.charAt(getRandomIndex(26));
+            password += capsStr.charAt(getRandomIndex(capsStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - numberOfIncludes); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -130,11 +143,11 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 2);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += specialCharStr.charAt(getRandomIndex(14));
+            password += specialCharStr.charAt(getRandomIndex(specialCharStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - numberOfIncludes); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
@@ -142,23 +155,23 @@ const generatePswd = () => {
         const numberOfIncludes = Math.floor(pswSize.value / 2);
 
         for(let i = 0; i < numberOfIncludes; i++) {
-            password += numberStr.charAt(getRandomIndex(10));
+            password += numberStr.charAt(getRandomIndex(numberStr.length - 1));
         }
 
         for(let i = 0; i < (pswSize.value - numberOfIncludes); i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     }
 
     if(!includeCaps && !includeSpeChar && !includeNumbers) {
         for(let i = 0; i < pswSize.value; i++) {
-            password += alphabet.charAt(getRandomIndex(26));
+            password += alphabet.charAt(getRandomIndex(alphabet.length - 1));
         }
     } else {
         // Function to shuffle $password
     }
 
-
+    password = shuffle(password);
     displayPsw.textContent = password;
     generatorButton.textContent = "Générez un nouveau mdp";
     passwordReturn.classList.remove('hide');
@@ -170,4 +183,3 @@ const generatePswd = () => {
 
 // Event 
 generatorButton.addEventListener('click', generatePswd);
-
